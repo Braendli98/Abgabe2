3a869e837bd478c87fd3efbe9b6635aa4b36da05
Meryem Daoussi <meryem.daoussi@gmail.com>
2025-01-01 17:05:05 +0100

    update: button color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,7 +31,7 @@ export default function Header() {
             />
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                    <Button className="bg-mainColor text-textGray hover:bg-mainColor hover:bg-opacity-20 bg-opacity-10 border-inputBorder">
+                    <Button className="bg-mainColor text-textGray hover:bg-mainColor hover:bg-opacity-60 bg-opacity-10 border-inputBorder">
                         <User />
                     </Button>
                 </DropdownMenuTrigger>

fe21a02c8eee4cac1cfd3bd173e5d2dfe8af53c0
AnastasiaBologan <148198988+AnastasiaBologan@users.noreply.github.com>
2025-01-07 21:31:16 +0100

    Update/ab/addbook2 (#21)
    * add: rating
    
    * update: eingabefelder vorschläge
    
    * update: rabatt optional rating_image
    
    * fix: prettier
    
    * update: stars_addbook*

--- src/components/add/AddBook.tsx
+++ src/components/add/AddBook.tsx
@@ -7,6 +7,7 @@ import { apiPost } from '@/lib/api/api-handler';
 import { useNavigate } from 'react-router';
 import { useState } from 'react';
 import { useToast } from '@/hooks/use-toast';
+import StarRating from '../common/StarRating';
 
 export default function AddBook() {
     const navigate = useNavigate();
@@ -31,14 +32,12 @@ export default function AddBook() {
         e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
     ) => {
         const { name, value, type, checked } = e.target as HTMLInputElement;
-        let fieldValue: string | boolean | string[];
+        let fieldValue: string | boolean | string[] = value;
 
         if (type === 'checkbox') {
             fieldValue = checked;
         } else if (name === 'schlagwoerter') {
             fieldValue = value.split(',');
-        } else {
-            fieldValue = value;
         }
 
         if (name.startsWith('titel.')) {
@@ -58,8 +57,71 @@ export default function AddBook() {
         }
     };
 
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setFormData((prev) => ({
+                    ...prev,
+                    coverImage: reader.result as string,
+                }));
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const handleStarClick = (newRating: number) => {
+        setFormData((prev) => ({
+            ...prev,
+            rating: Math.floor(newRating),
+        }));
+    };
+
+    const validateForm = () => {
+        if (!formData.titel.titel || formData.titel.titel.trim() === '') {
+            return 'Titel ist erforderlich.';
+        }
+
+        if (!formData.isbn || formData.isbn.trim() === '') {
+            return 'ISBN ist erforderlich.';
+        }
+
+        if (!/^\d{3}-\d{1,5}-\d{1,7}-\d{1,7}-\d+$/.test(formData.isbn)) {
+            return 'Die ISBN ist ungültig. Sie muss dem Format "978-1-56619-909-6" entsprechen.';
+        }
+
+        if (formData.preis <= 0) {
+            return 'Der Preis muss größer als 0 sein.';
+        }
+
+        if (formData.rabatt < 0 || formData.rabatt > 100) {
+            return 'Der Rabatt muss zwischen 0 und 100 liegen.';
+        }
+
+        if (!formData.datum || formData.datum.trim() === '') {
+            return 'Ein Datum ist erforderlich.';
+        }
+
+        if (new Date(formData.datum).toString() === 'Invalid Date') {
+            return 'Ein gültiges Datum ist erforderlich.';
+        }
+
+        return null;
+    };
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        const errorMessage = validateForm();
+        if (errorMessage) {
+            toast({
+                title: 'Ungültige Eingabe',
+                description: errorMessage,
+                variant: 'destructive',
+            });
+            return;
+        }
 
         const formattedDate = new Date(formData.datum)
             .toISOString()
@@ -69,8 +131,13 @@ export default function AddBook() {
             ...formData,
             datum: formattedDate,
             preis: parseFloat(formData.preis.toString()),
-            rabatt: parseFloat(formData.rabatt.toString()),
+            rabatt: parseFloat(formData.rabatt.toString()) / 100,
         };
+
+        console.log(
+            'Gesendete Daten:',
+            JSON.stringify(submissionData, null, 2),
+        );
 
         apiPost(
             '/api/rest',
@@ -94,55 +161,59 @@ export default function AddBook() {
             >
                 <div className="flex flex-col space-y-4">
                     <label className="font-medium">
-                        Titel:
+                        Titel<span className="text-red-500">*</span>:{' '}
                         <input
                             type="text"
                             name="titel.titel"
                             value={formData.titel.titel}
                             onChange={handleChange}
                             className="border rounded-md p-2 w-full"
+                            placeholder="Der Herr der Ringe"
                             required
                         />
                     </label>
 
                     <label className="font-medium">
-                        Untertitel:
+                        Untertitel:{' '}
                         <input
                             type="text"
                             name="titel.untertitel"
                             value={formData.titel.untertitel}
                             onChange={handleChange}
                             className="border rounded-md p-2 w-full"
+                            placeholder="Die Gefährten"
                         />
                     </label>
 
                     <label className="font-medium">
-                        ISBN:
+                        ISBN<span className="text-red-500">*</span>:{' '}
                         <input
                             type="text"
                             name="isbn"
                             value={formData.isbn}
                             onChange={handleChange}
                             className="border rounded-md p-2 w-full"
+                            placeholder="978-3-86680-192-9"
                             required
                         />
                     </label>
 
                     <label className="font-medium">
-                        Datum:
+                        Datum<span className="text-red-500">*</span>:{' '}
                         <input
                             type="date"
                             name="datum"
                             value={formData.datum}
                             onChange={handleChange}
                             className="border rounded-md p-2 w-full"
+                            onKeyDown={(e) => e.preventDefault()}
                         />
                     </label>
 
                     <label className="font-medium">
-                        Preis (€):
+                        Preis (€)<span className="text-red-500">*</span>:{' '}
                         <input
-                            type="number"
+                            type="text"
                             name="preis"
                             value={formData.preis}
                             onChange={handleChange}
@@ -152,12 +223,34 @@ export default function AddBook() {
                     </label>
 
                     <label className="font-medium">
-                        Rabatt (%):
+                        Rabatt (%):{' '}
                         <input
                             type="number"
                             name="rabatt"
                             value={formData.rabatt}
                             onChange={handleChange}
+                            className="border rounded-md p-2 w-full"
+                        />
+                    </label>
+
+                    <label className="font-medium">
+                        Rating:{' '}
+                        <div className="mt-2">
+                            <StarRating
+                                rating={formData.rating}
+                                onClick={(newRating: number) =>
+                                    handleStarClick(newRating)
+                                }
+                            />
+                        </div>
+                    </label>
+
+                    <label className="font-medium">
+                        Cover Image:{' '}
+                        <input
+                            type="file"
+                            accept="image/*"
+                            onChange={handleFileChange}
                             className="border rounded-md p-2 w-full"
                         />
                     </label>
@@ -199,7 +292,7 @@ export default function AddBook() {
                     </div>
 
                     <label className="font-medium">
-                        Lieferbar:
+                        Lieferbar:{' '}
                         <input
                             type="checkbox"
                             name="lieferbar"
@@ -210,14 +303,14 @@ export default function AddBook() {
                     </label>
 
                     <label className="font-medium">
-                        Schlagwörter:
+                        Schlagwörter:{' '}
                         <input
                             type="text"
                             name="schlagwoerter"
                             value={formData.schlagwoerter.join(',')}
                             onChange={handleChange}
                             className="border rounded-md p-2 w-full"
-                            placeholder="Comma-separated"
+                            placeholder="Fantasy, Abenteuer"
                         />
                     </label>
                 </div>
